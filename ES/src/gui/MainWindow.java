e2443baa25c5f573493fbd6ebee3717360ee7f44
cvbhs <cvbhs@iscte-iul.pt>
2018-09-29 21:13:58 +0100

    Estrutura vagamente definida

--- ES/src/gui/MainWindow.java
+++ ES/src/gui/MainWindow.java
@@ -1,57 +1,84 @@
 package gui;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class MainWindow extends Application{
-	
+public class MainWindow extends Application {
+
 	Stage windowMain;
 	Scene sceneDefault, sceneMail, sceneFB, sceneTwitter;
 	Button buttonDefault, buttonFB, buttonMail, buttonTwitter, buttonSettings;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// Stage
 		windowMain = primaryStage;
 		windowMain.setTitle("Bom Dia Academia");
 
-		//Botões
+		// Buttons
 		buttonDefault = new Button("Home");
 		buttonDefault.setOnAction(e -> windowMain.setScene(sceneDefault));
-		
+
 		buttonMail = new Button("Mail");
 		buttonMail.setOnAction(e -> windowMain.setScene(sceneMail));
-		
+
 		buttonFB = new Button("FaceBook");
 		buttonFB.setOnAction(e -> windowMain.setScene(sceneFB));
-		
+
 		buttonTwitter = new Button("Twitter");
 		buttonTwitter.setOnAction(e -> windowMain.setScene(sceneTwitter));
-		
+
 		buttonSettings = new Button("Settings");
 		buttonSettings.setOnAction(e -> SettingsWindow.openSettings());
-		
-		
-		
-		HBox layout = new HBox(10);
-		layout.getChildren().add(buttonDefault);
-		layout.getChildren().add(buttonFB);
-		layout.getChildren().add(buttonMail);
-		layout.getChildren().add(buttonTwitter);
-		layout.getChildren().add(buttonSettings);
-		layout.setAlignment(Pos.TOP_CENTER);
-		
-		sceneDefault = new Scene(layout, 1000, 600);
+
+		// Scenes
+		sceneGetDefault();
+
+		// Stage Show
 		windowMain.setScene(sceneDefault);
 		windowMain.show();
 	}
 
+	private void sceneGetDefault() {
+		BorderPane frame = new BorderPane();
+		HBox buttonsTop = buttonsTop();
+		frame.setTop(buttonsTop);
+
+		try {
+			Image imageIscte = new Image(new FileInputStream("src/gui/iscte.png"));
+			ImageView imageViewIscte = new ImageView(imageIscte);
+			frame.setCenter(imageViewIscte);
+		} catch (FileNotFoundException e) {
+			System.out.println("Erro no path da Imagem ISCTE");
+		}
+
+		sceneDefault = new Scene(frame, 1000, 600);
+	}
+
+	private HBox buttonsTop() {
+		HBox buttonsTop = new HBox(10);
+		buttonsTop.getChildren().add(buttonDefault);
+		buttonsTop.getChildren().add(buttonFB);
+		buttonsTop.getChildren().add(buttonMail);
+		buttonsTop.getChildren().add(buttonTwitter);
+		buttonsTop.getChildren().add(buttonSettings);
+		buttonsTop.setAlignment(Pos.CENTER);
+		return buttonsTop;
+	}
+
 }
-
-
--- ES/src/gui/SettingsWindow.java
+++ ES/src/gui/SettingsWindow.java
@@ -1,16 +1,48 @@
 package gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SettingsWindow {
 	
-	Stage windowSettings;
-	Scene sceneMailSettings, sceneFBSettings, sceneTwitterSettings;
 	
-	public static Object openSettings() {
-		// TODO Auto-generated method stub
-		return null;
+	static Scene sceneMailSettings, sceneFBSettings, sceneTwitterSettings;
+	
+	public static void openSettings() {
+		//Stage
+		Stage windowSettings = new Stage();
+		windowSettings.setTitle("Settings");
+		windowSettings.initModality(Modality.APPLICATION_MODAL);
+		windowSettings.setMinHeight(400);
+		windowSettings.setMinWidth(800);
+		
+		//Buttons
+		Button  buttonMailSettings = new Button("Mail");
+		buttonMailSettings.setOnAction(e -> windowSettings.setScene(sceneMailSettings));
+		
+		Button buttonFBSettings = new Button("FaceBook");
+		buttonFBSettings.setOnAction(e -> windowSettings.setScene(sceneFBSettings));
+		
+		Button buttonTwitterSettings = new Button("Twitter");
+		buttonTwitterSettings.setOnAction(e -> windowSettings.setScene(sceneTwitterSettings));
+		
+		//Scenes
+		//Mail Settings
+		VBox layout = new VBox(10);
+		layout.getChildren().add(buttonMailSettings);
+		layout.getChildren().add(buttonFBSettings);
+		layout.getChildren().add(buttonTwitterSettings);
+		layout.setAlignment(Pos.CENTER_LEFT);
+		sceneMailSettings = new Scene(layout);
+		
+		//Stage Show
+		windowSettings.setScene(sceneMailSettings);
+		windowSettings.showAndWait();
+		
 	}
 
 }
