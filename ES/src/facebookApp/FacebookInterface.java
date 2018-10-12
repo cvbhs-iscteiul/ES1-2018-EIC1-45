acd2968f9aa578a91852780ffc278877d377c0a1
F0r Fr33 <F0r Fr33@F0rFr33>
2018-10-12 13:57:51 +0100

    Must finish interface- Left:Test RestfB API

--- ES/src/facebookApp/FacebookInterface.java
+++ ES/src/facebookApp/FacebookInterface.java
@@ -3,6 +3,11 @@ package facebookApp;
 public interface FacebookInterface {
 
 	
-	
+	void connectToFacebook();//needs args like Token
+	void getFeed();
+	void filterFeed(); //args like time
+	//Page returnFilteredFeed();
+	void postNewPost();
+	void postReply();
 	
 }
